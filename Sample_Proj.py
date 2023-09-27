37476bb9d93a8edb736a2cbc96760b862a635ddb
Baaqar Naqi <110708987+Baaqar-007@users.noreply.github.com>
2023-09-27 13:27:17 +0530

    Add files via upload

---
+++ Sample_Proj.py
@@ -0,0 +1,36 @@
+import pyperclip, re
+phoneRegex = re.compile(r'''(
+(\d{3}|\(\d{3}\))? # area code
+(\s|-|\.)? # separator
+(\d{3}) # first 3 digits
+(\s|-|\.) # separator
+(\d{4}) # last 4 digits
+(\s*(ext|x|ext.)\s*(\d{2,5}))? # extension
+)''', re.VERBOSE)
+
+emailRegex  =re.compile(r'''(
+[a-zA-Z0-9._%+-]+ # username
+@ # @ symbol
+[a-zA-Z0-9.-]+ # domain name
+(\.[a-zA-Z]{2,4}) # dot-something
+)''', re.VERBOSE) 
+
+
+text = str(pyperclip.paste())
+matches = []
+for grps in phoneRegex.findall(text):
+    phonenum = '-'.join([grps[1],grps[3],grps[5]]) 
+    if grps[8] != ' ':
+        phonenum += ' x'+ grps[8]
+    matches.append(phonenum)
+
+for groups in emailRegex.findall(text):
+    matches.append(groups[0])
+
+if len(matches) > 0:
+    pyperclip.copy('\n'.join(matches))
+    print('Copied to clipboard:')
+    print('\n'.join(matches))
+else:
+    print('No phone numbers or email addresses found.')
+
